57e0f68d4339c9dfce85bc7cf1c32a6697b59365
Zachary Leinweber <zleinweb@gmail.com>
2020-10-24 22:33:16 -0500

    Add project static and templates dir

--- swiftroad/swiftroad/settings.py
+++ swiftroad/swiftroad/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "swiftroad.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,5 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 STATIC_URL = "/static/"
